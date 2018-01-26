defc3fb7ca564d18e5687797f7f8627ed96a6fcb
manojeminem <35653185+manojeminem@users.noreply.github.com>
2018-01-26 16:14:32 +0530

    Create m.c

---
+++ m.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main()
+{
+    char bala;
+    printf("enter the alphabet");
+    if(a>=a&&a<=z);
+    scanf("%ch",bala);
+    else
+    printf("enter not the alphabet");
+    getch();
+}
